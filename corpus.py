8ac1f3fc56342411e3da314d5c642e837ec87f13
Brian Campbell <briancampbell@lucidchart.com>
2020-03-26 22:05:06 -0700

    Change corpus generation to use maths
    Attempts to identify which skills are uniquely interested in which
    channels

--- corpus.py
+++ corpus.py
@@ -2,6 +2,8 @@ from cachier import cachier
 from pathlib import Path
 import slack
 import json
+from collections import Counter, defaultdict
+import pandas as pd
 
 
 slack_channel = "CUXD81R6X"
@@ -10,7 +12,9 @@ api = slack.WebClient(slack_token)
 
 
 def build_corpus(skills, introductions, channels):
-    corpus, relations = {}, []
+    corpus, relations = {}, []  
+    general_channels = set(['announcements', 'discussion-resources', 'questions-support-feedback', 'casual-discussion', 'introductions', 'media-published-articles'])
+    channels = [c for c in channels if len(c['members']) > 10 and c['name'] not in general_channels]
 
     for user, introduction in introductions.items():
         relations += [[introduction.casefold(), [
@@ -21,14 +25,25 @@ def build_corpus(skills, introductions, channels):
     for introduction, channels in relations:
         for channel in channels:
             if not corpus.get(channel):
-                corpus[channel] = set()
+                corpus[channel] = {}
             for token in tokenize(skills, introduction):
-                corpus[channel].add(token)
+                corpus[channel][token] = corpus[channel].get(token, 0) + 1
+    
+    df = pd.DataFrame(corpus)
+    row_maxes = df.max(axis=1)
+    normal_row = df.T.divide(row_maxes).T
+    col_maxes = normal_row.max()
+    final_df = normal_row.divide(col_maxes)
 
-    for channel, skills in corpus.items():
-        corpus[channel] = list(filter(None, skills))
-
-    return corpus
+    channel_keywords = defaultdict(set)
+    for row in final_df.iterrows():
+        keyword_info = row[1].nlargest(4)
+        keyword = keyword_info.name
+        keyword_channels = keyword_info.index.to_list()
+        for channel in keyword_channels:
+            channel_keywords[channel].add(keyword)
+    channel_keywords = {c:list(k) for c,k in channel_keywords.items()}
+    return channel_keywords
 
 
 def tokenize(skills, text):
@@ -69,7 +84,7 @@ def fetch_members(channel):
 
 
 with open(Path(__file__).parent / "input" / "skills.txt", "r") as skill_file:
-    with open(Path(__file__).parent / "output" / corpus.json", "w") as corpus_file:
+    with open(Path(__file__).parent / "output" / "corpus.json", "w") as corpus_file:
         corpus = build_corpus(
             skill_file.read().split("\n"),
             fetch_introductions(slack_channel),
